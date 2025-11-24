2afe16903782281c6081dd3c2575f5eeec14e9ec
Rishita Chakravorty <chakravorty.r20@outlook.com>
2025-11-24 11:01:25 +0530

    Implement blog functionality with database models, routes, and CRUD operations

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,33 @@
-from fastapi import FastAPI
-from . import schemas
+from fastapi import FastAPI, Depends
+from . import schemas, models
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post('/blog')
-def create(request: schemas.Blog):
-    return {'data' : {
-        'title' : request.title,
-        'body' : request.body
-    }}
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all() # returns all the blogs
+    return blogs
+
+@app.get('/blog/{id}')
+def show(id, db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first() # withour first this doesnt work throws error
+    return blog
